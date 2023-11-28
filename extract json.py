211e427b70b1f6eb89a3e51cb02e8e1d0f74936f
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-11-28 13:49:32 -0800

    Update extract json.py

--- extract json.py
+++ extract json.py
@@ -1,30 +1,13 @@
 import json
 
-def extract_treatment_area(json_data):
-    treatment_areas = []
+# Load JSON data from the file
+with open('Appended.json', 'r') as file:
+    data = json.load(file)
 
-    # Iterate through the points in the JSON data
-    for point in json_data.get('points', []):
-        # Check if the necessary nested keys exist
-        if 'head' in point and 'position' in point['head']:
-            position = point['head']['position']
-            
-            # Check if 'treatment_area' key exists in the position dictionary
-            if 'treatment_area' in position and isinstance(position['treatment_area'], bool):
-                treatment_areas.append(position['treatment_area'])
-
-    return treatment_areas
-
-def main():
-    # Load JSON data from the file
-    with open('appended.json', 'r') as file:
-        json_data = json.load(file)
-
-    # Extract treatment_area booleans
-    treatment_areas = extract_treatment_area(json_data)
-
-    # Print the result
-    print("Treatment Areas:", treatment_areas)
-
-if __name__ == "__main__":
-    main()
+# Access 'treatment_area' values
+for point in data.get('points', []):
+    treatment_area_value = point.get('treatment_area')
+    if treatment_area_value is not None:
+        print(treatment_area_value)
+    else:
+        print("Treatment area not found for a point.")
