00627d6fbcadfb24296ec19ae52124cd3b845d4c
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-11-28 14:25:58 -0800

    Update extract json test.py

--- extract json test.py
+++ extract json test.py
@@ -4,7 +4,8 @@ import json
 with open('Appended.json', 'r') as file:
     data = json.load(file)
 
-# Initialize variables
+# Initialize a variable to track whether 'start' should be captured
+capture_start = False
 start_data = []
 
 # Access 'treatment_area' values
@@ -12,18 +13,17 @@ for point in data.get('points', []):
     treatment_area_value = point.get('treatment_area')
 
     if treatment_area_value is not None:
-        if not start_data and treatment_area_value == False:
+        if not capture_start and treatment_area_value == False:
             # Found the first 'false' point, start capturing data
-            start_data.append(treatment_area_value)
+            capture_start = True
 
-        if start_data:
-            # Continue capturing treatment_area values until the first 'true' point
-            start_data.append(treatment_area_value)
+        if capture_start:
+            # Capture the data
+            start_data.append(point)
 
-        if start_data and treatment_area_value == True:
+        if capture_start and treatment_area_value == True:
             # Found the first 'true' treatment area point, stop capturing data
             break
 
 # Print the captured 'start' data
 print(start_data)
-
