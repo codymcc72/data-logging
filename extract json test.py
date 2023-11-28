69ad0b90321101b705822296659bdf2daeecebe6
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-11-28 14:54:02 -0800

    Update extract json test.py

--- extract json test.py
+++ extract json test.py
@@ -4,26 +4,31 @@ import json
 data = json.loads(json_data)
 
 # Initialize variables
-start_data = []
-row_data = []
+all_data = []
 capturing_start = True  # Flag to determine whether to capture 'start' or 'row' data
 
-# Access 'treatment_area' values
+# Access 'treatment_area' values for each set
 for point in data.get('points', []):
     treatment_area_value = point.get('treatment_area')
 
     if capturing_start:
         if treatment_area_value is not None and treatment_area_value is False:
-            start_data.append(treatment_area_value)
-        elif start_data:
+            start_data = [treatment_area_value]
             capturing_start = False
-
+        elif not capturing_start:
+            if treatment_area_value is not None and treatment_area_value is True:
+                row_data = [treatment_area_value]
+                capturing_start = True
+                all_data.append({"start": start_data, "row": row_data})
     else:
         if treatment_area_value is not None and treatment_area_value is True:
             row_data.append(treatment_area_value)
         elif row_data:
-            break  # Stop capturing 'row' data when the next 'false' is encountered
+            capturing_start = True
+            all_data.append({"start": start_data, "row": row_data})
 
-# Print the captured 'start' data and 'row' data
-print("Start Data:", start_data)
-print("Row Data:", row_data)
+# Print all the captured data
+for idx, data_set in enumerate(all_data, start=1):
+    print(f"Set {idx} - Start Data:", data_set["start"])
+    print(f"Set {idx} - Row Data:", data_set["row"])
+    print()
