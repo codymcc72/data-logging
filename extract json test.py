f7938af547d88ea936fff8c35b752633b37968f0
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-11-29 09:35:14 -0800

    Update extract json test.py

--- extract json test.py
+++ extract json test.py
@@ -1,34 +1,28 @@
 import json
 
-
 # Initialize variables
-start_data = []
+all_rows_data = []
+
 with open('Appended.json', 'r') as file:
     data = json.load(file)
+
 # Access 'treatment_area' values
 for point in data.get('points', []):
     treatment_area_value = point.get('treatment_area')
 
     if treatment_area_value is not None:
-        if not start_data and treatment_area_value == False:
-            # Found the first 'false' point, start capturing data
+        if treatment_area_value is False:
+            # Found the start of a new row, initialize variables
+            start_data = [treatment_area_value]
+            row_data = []
+        elif start_data:
+            # Continue capturing treatment_area values until the next 'True' point
             start_data.append(treatment_area_value)
+            if treatment_area_value is True:
+                row_data = start_data.copy()
+                all_rows_data.append(row_data)
+                start_data = []
 
-        if start_data:
-            # Continue capturing treatment_area values until the first 'true' point
-            start_data.append(treatment_area_value)
-
-        if start_data and treatment_area_value == True:
-            # Found the first 'true' treatment area point, stop capturing data
-            break
-
-# Print the captured 'start' data
-print(start_data)
-
-
-Output:
-
-[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True]
-
-
-Need to repeat the process for each row
+# Print the captured data for each row
+for idx, row_data in enumerate(all_rows_data, start=1):
+    print(f"Row {idx} Data:", row_data)
