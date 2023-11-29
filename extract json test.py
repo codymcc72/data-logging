00fc284ae5562c535a3b905bc160a5921e8a6605
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-11-29 09:30:11 -0800

    Update extract json test.py

--- extract json test.py
+++ extract json test.py
@@ -1,31 +1,32 @@
 import json
 
-# Assuming json_data contains your JSON data
-data = json.loads(json_data)
+# Specify the path to your JSON file
+json_file_path = 'path/to/your/file.json'
 
 # Initialize variables
 all_data = []
 capturing_start = True  # Flag to determine whether to capture 'start' or 'row' data
+start_data = []  # Initialize 'start_data' outside the loop
 
-# Access 'treatment_area' values for each set
-for point in data.get('points', []):
-    treatment_area_value = point.get('treatment_area')
+# Open the JSON file using 'with open()' method
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
 
-    if capturing_start:
-        if treatment_area_value is not None and treatment_area_value is False:
-            start_data = [treatment_area_value]
-            capturing_start = False
-        elif not capturing_start:
+    # Access 'treatment_area' values for each set
+    for point in data.get('points', []):
+        treatment_area_value = point.get('treatment_area')
+
+        if capturing_start:
+            if treatment_area_value is not None and treatment_area_value is False:
+                start_data = [treatment_area_value]
+                capturing_start = False
+        else:
             if treatment_area_value is not None and treatment_area_value is True:
                 row_data = [treatment_area_value]
                 capturing_start = True
                 all_data.append({"start": start_data, "row": row_data})
-    else:
-        if treatment_area_value is not None and treatment_area_value is True:
-            row_data.append(treatment_area_value)
-        elif row_data:
-            capturing_start = True
-            all_data.append({"start": start_data, "row": row_data})
+            elif row_data:
+                row_data.append(treatment_area_value)
 
 # Print all the captured data
 for idx, data_set in enumerate(all_data, start=1):
